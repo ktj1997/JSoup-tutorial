e44fece441747e75571f2abd1beb312d194f720e
김태준(taejukim) <taejukim@gmarket.com>
2023-01-30 21:01:22 +0900

    project init

---
+++ src/main/kotlin/com/example/jsoupcrawler/JsoupCrawlerApplication.kt
@@ -0,0 +1,11 @@
+package com.example.jsoupcrawler
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JsoupCrawlerApplication
+
+fun main(args: Array<String>) {
+    runApplication<JsoupCrawlerApplication>(*args)
+}
